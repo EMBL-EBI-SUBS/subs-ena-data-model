b46420faf423948449965a55c8b5fb23654302bc
Neil Goodgame <neilg@ebi.ac.uk>
2017-07-10 15:19:03 +0100

    Removed attributeValue and added getter for allowedValues

--- src/main/java/uk/ac/ebi/subs/ena/validation/InvalidAttributeValue.java
+++ src/main/java/uk/ac/ebi/subs/ena/validation/InvalidAttributeValue.java
@@ -9,7 +9,6 @@ import uk.ac.ebi.subs.data.submittable.ENASubmittable;
 public class InvalidAttributeValue extends AbstractENAValidationResult {
     static final String INVALID_VALUE_ERROR_MESSAGE = "Invalid value %s for attribute %s value must be one of %s.";
 
-    String attributeValue;
     String[] allowedValues;
 
     public InvalidAttributeValue(ENASubmittable enaSubmittable, String invalidValue, String fieldName, String [] allowedValues) {
@@ -21,11 +20,10 @@ public class InvalidAttributeValue extends AbstractENAValidationResult {
                         StringUtils.join(allowedValues, ",")
                 )
         );
-        this.attributeValue = attributeValue;
         this.allowedValues = allowedValues;
     }
 
-    public String getAttributeValue() {
-        return attributeValue;
+    public String[] getAllowedValues() {
+        return allowedValues;
     }
 }
