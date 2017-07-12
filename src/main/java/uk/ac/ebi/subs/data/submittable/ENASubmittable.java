7fca34ca8bfdc87516337bd6aecdb7e70f682ceb
Neil Goodgame <neilg@ebi.ac.uk>
2017-07-12 10:10:04 +0100

    Added getValidationResultList and isValid to the ENASubmittable interface

--- src/main/java/uk/ac/ebi/subs/data/submittable/ENASubmittable.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/ENASubmittable.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.subs.data.submittable;
 
 import uk.ac.ebi.subs.data.component.Attribute;
+import uk.ac.ebi.subs.validator.data.SingleValidationResult;
 
 import java.util.List;
 
@@ -8,9 +9,31 @@ import java.util.List;
  * Created by neilg on 09/04/2017.
  */
 public interface ENASubmittable<T extends Submittable> extends Submittable {
+    /**
+     * Gets the Submittable object that underpines this object
+     * @return
+     */
     T getBaseObject ();
+
+    /**
+     * Serialises attributes from a Submittable to fields in an ENASubmittable.
+     * @throws IllegalAccessException
+     */
     void serialiseAttributes () throws IllegalAccessException;
+
+    /**
+     * Deserialises fields from an ENASubmittable to attributes in a Submittable
+     * @throws IllegalAccessException
+     */
     void deSerialiseAttributes () throws IllegalAccessException;
-    List<Attribute> getAttributesXML();
-    void setAttributesXML(List<Attribute> attributeList);
+    /**
+     * Classes that implement this interface should create a list of SingleValidationResult objects
+     * during serialisation to represent any validation errors
+     */
+    List<SingleValidationResult> getValidationResultList();
+
+    /**
+     * @return true if any validation errors occur during serialisation
+     */
+    boolean isValid();
 }
