15cbd560b812bce4e462790b6ced1fc1d8bb7d14
Neil Goodgame <neilg@ebi.ac.uk>
2017-08-08 11:16:10 +0100

    Updated the instrument_model to constants

--- src/main/java/uk/ac/ebi/subs/data/submittable/ENAExperiment.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/ENAExperiment.java
@@ -56,38 +56,38 @@ public class    ENAExperiment extends AbstractENASubmittable<Assay> {
 
 
 
-    @ENAField(fieldName = LS454, attributeName = "instrument_model" ,values = {"454 GS 20", "454 GS FLX", "454 GS FLX+", "454 GS FLX Titanium", "454 GS Junior", "unspecified"})
+    @ENAField(fieldName = LS454, attributeName = INSTRUMENT_MODEL ,values = {"454 GS 20", "454 GS FLX", "454 GS FLX+", "454 GS FLX Titanium", "454 GS Junior", "unspecified"})
     String ls454 ;
 
-    @ENAField(fieldName = ILLUMINA, attributeName = "instrument_model" ,values = {"Illumina Genome Analyzer", "Illumina Genome Analyzer II", "Illumina Genome Analyzer IIx",
+    @ENAField(fieldName = ILLUMINA, attributeName = INSTRUMENT_MODEL ,values = {"Illumina Genome Analyzer", "Illumina Genome Analyzer II", "Illumina Genome Analyzer IIx",
             "Illumina HiSeq 2500", "Illumina HiSeq 2000", "Illumina HiSeq 1500", "Illumina HiSeq 1000", "Illumina MiSeq", "Illumina HiScanSQ",
             "HiSeq X Ten", "NextSeq 500", "HiSeq X Five", "Illumina HiSeq 3000", "Illumina HiSeq 4000", "NextSeq 550", "unspecified"})
     String illumina ;
 
-    @ENAField(fieldName = HELICOS, attributeName = "instrument_model" ,values = {"Helicos HeliScope", "unspecified"})
+    @ENAField(fieldName = HELICOS, attributeName = INSTRUMENT_MODEL ,values = {"Helicos HeliScope", "unspecified"})
     String helicos;
 
-    @ENAField(fieldName = ABI_SOLID, attributeName = "instrument_model" ,values = {"AB SOLiD System 2.0", "AB SOLiD System 3.0", "AB SOLiD 3 Plus System",
+    @ENAField(fieldName = ABI_SOLID, attributeName = INSTRUMENT_MODEL ,values = {"AB SOLiD System 2.0", "AB SOLiD System 3.0", "AB SOLiD 3 Plus System",
             "AB SOLiD 4 System", "AB SOLiD 4hq System", "AB SOLiD PI System", "AB 5500 Genetic Analyzer", "AB 5500xl Genetic Analyzer",
             "AB 5500xl-W Genetic Analysis System", "unspecified"})
     String abiSolid ;
 
-    @ENAField(fieldName = COMPLETE_GENOMICS, attributeName = "instrument_model" ,values = {"Complete Genomics", "unspecified"})
+    @ENAField(fieldName = COMPLETE_GENOMICS, attributeName = INSTRUMENT_MODEL ,values = {"Complete Genomics", "unspecified"})
     String completeGenomics;
 
-    @ENAField(fieldName = BGISEQ, attributeName = "instrument_model" ,values = {"BGISEQ-500"})
+    @ENAField(fieldName = BGISEQ, attributeName = INSTRUMENT_MODEL ,values = {"BGISEQ-500"})
     String bgiseq;
 
-    @ENAField(fieldName = OXFORD_NANOPORE, attributeName = "instrument_model" ,values = {"MinION", "GridION", "unspecified"})
+    @ENAField(fieldName = OXFORD_NANOPORE, attributeName = INSTRUMENT_MODEL ,values = {"MinION", "GridION", "unspecified"})
     String oxfordNanopore;
 
-    @ENAField(fieldName = PACBIO_SMRT, attributeName = "instrument_model" ,values = {"PacBio RS", "PacBio RS II", "Sequel", "unspecified"})
+    @ENAField(fieldName = PACBIO_SMRT, attributeName = INSTRUMENT_MODEL ,values = {"PacBio RS", "PacBio RS II", "Sequel", "unspecified"})
     String pacbioSMRT;
 
-    @ENAField(fieldName = ION_TORRENT, attributeName = "instrument_model" ,values = {"Ion Torrent PGM", "Ion Torrent Proton", "unspecified"})
+    @ENAField(fieldName = ION_TORRENT, attributeName = INSTRUMENT_MODEL ,values = {"Ion Torrent PGM", "Ion Torrent Proton", "unspecified"})
     String ionTorrent;
 
-    @ENAField(fieldName = CAPILLARY, attributeName = "instrument_model" ,values = {"AB 3730xL Genetic Analyzer", "AB 3730 Genetic Analyzer", "AB 3500xL Genetic Analyzer",
+    @ENAField(fieldName = CAPILLARY, attributeName = INSTRUMENT_MODEL ,values = {"AB 3730xL Genetic Analyzer", "AB 3730 Genetic Analyzer", "AB 3500xL Genetic Analyzer",
             "AB 3500 Genetic Analyzer", "AB 3130xL Genetic Analyzer", "AB 3130 Genetic Analyzer", "AB 3130 Genetic Analyzer", "AB 310 Genetic Analyzer",
             "unspecified"})
     String capillary;
@@ -126,20 +126,6 @@ public class    ENAExperiment extends AbstractENASubmittable<Assay> {
     public ENAExperiment() throws IllegalAccessException {
         super();
     }
-
-    /*
-    public Map<String,String[]> getPlatformInstrumentMap () {
-        Map<String,String[]> platformInstrumentMap = new HashMap<>();
-        final Field[] declaredFields = this.getClass().getDeclaredFields();
-
-        for (Field field : declaredFields) {
-            if (field.isAnnotationPresent(ENAPlatform.class)) {
-                final ENAPlatform annotation = field.getAnnotation(ENAPlatform.class);
-                platformInstrumentMap.put(annotation.name(),annotation.instrumentModels());
-            }
-        }
-    }
-    */
 
     @Override
     public void serialiseAttributes() throws IllegalAccessException {
