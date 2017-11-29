c28d6c1561514f7cded64994b3791129a718222f
Neil Goodgame <neilg@ebi.ac.uk>
2017-11-29 13:37:11 +0000

    Renamed fieldName to ENAField.name

--- src/main/java/uk/ac/ebi/subs/ena/annotation/ENAField.java
+++ src/main/java/uk/ac/ebi/subs/ena/annotation/ENAField.java
@@ -14,7 +14,6 @@ public @interface ENAField {
     String TYPE_FIELD_NAME = "Type_Field_Name";
     String NO_ATTRIBUTE_TYPE = "No_Attribute_Type";
     String [] values() default {};
-    String fieldName() default TYPE_FIELD_NAME;
+    String name() default TYPE_FIELD_NAME;
     String attributeName() default NO_ATTRIBUTE_TYPE;
-    boolean copyValues() default true;
 }
