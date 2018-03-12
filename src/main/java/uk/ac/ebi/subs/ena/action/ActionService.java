000c3ba4f5e196f28fab2683c79acc431c112e94
Neil Goodgame <neilg@ebi.ac.uk>
2018-03-12 16:47:38 +0000

    Added getName

--- src/main/java/uk/ac/ebi/subs/ena/action/ActionService.java
+++ src/main/java/uk/ac/ebi/subs/ena/action/ActionService.java
@@ -5,4 +5,5 @@ import uk.ac.ebi.ena.sra.xml.SubmissionType;
 public interface ActionService<T> {
     SubmissionType.ACTIONS.ACTION createActionXML (T submittableObject) throws IllegalArgumentException;
     Class<T> getSubmittableClass ();
+    String getName();
 }
