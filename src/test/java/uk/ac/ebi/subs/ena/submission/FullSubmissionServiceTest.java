e5519158be9c14bf0170284b0ce468a9eb2f5179
neilg <neilg@ebi.ac.uk>
2018-03-13 08:32:37 +0000

    Added test for runs based on the existing SRARunLoader test that uploads a test file via FTP

--- src/test/java/uk/ac/ebi/subs/ena/submission/FullSubmissionServiceTest.java
+++ src/test/java/uk/ac/ebi/subs/ena/submission/FullSubmissionServiceTest.java
@@ -1,9 +1,13 @@
 package uk.ac.ebi.subs.ena.submission;
 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.ac.ebi.ena.sra.xml.RECEIPTDocument;
@@ -15,8 +19,13 @@ import uk.ac.ebi.subs.ena.action.AssayActionService;
 import uk.ac.ebi.subs.ena.action.SampleActionService;
 import uk.ac.ebi.subs.ena.action.StudyActionService;
 import uk.ac.ebi.subs.ena.helper.TestHelper;
-import org.apache.commons.lang.SerializationUtils;
+import uk.ac.ebi.subs.data.component.File;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -26,6 +35,20 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {EnaAgentApplication.class})
 public class FullSubmissionServiceTest {
+    @Value("${ena.ftp.url}")
+    String enaFTPServerURL;
+
+    @Value("${ena.login_name}")
+    String ftpUsername;
+
+    @Value("${ena.password}")
+    String ftpPassword;
+
+    private String FASTQ_FILE_NAME = "test_forward_" + UUID.randomUUID().toString() + ".gz";
+
+    private String FASTQ_FILE = "src/test/resources/uk/ac/ebi/subs/ena/" + FASTQ_FILE_NAME;
+
+    private File fastQFile;
 
     public static final int SUBMITTABLE_COUNT = 10;
     @Autowired
@@ -34,7 +57,7 @@ public class FullSubmissionServiceTest {
     Study[] studies;
     Sample[] samples;
     Assay[] assays;
-    AssayData assayData;
+    AssayData[] assayDatas;
     String submissionAlias;
     Team team;
     Map<Class<? extends ActionService>,Object> parameterMap = new HashMap<>();
@@ -61,6 +84,7 @@ public class FullSubmissionServiceTest {
             samples[i] = TestHelper.getSample(UUID.randomUUID().toString(),team);
             assays[i] = TestHelper.getAssay(UUID.randomUUID().toString(),team,samples[i].getAlias(),studies[0].getAlias());
         }
+
     }
 
     @Test
@@ -73,7 +97,6 @@ public class FullSubmissionServiceTest {
         }
     }
 
-    //@Test
     public void submitAndUpdateStudies() throws Exception {
         Study [] modifiedStudies = new Study[studies.length];
 
@@ -115,6 +138,79 @@ public class FullSubmissionServiceTest {
         for (Submittable submittable : assays) {
             assertThat(submittable.getAccession(), startsWith("ERX"));
         }
+    }
+
+    //@Test
+    public void submitStudySamplesExperimentAndRun() throws Exception {
+        FTPClient ftpClient = connectToWebinFTP();
+        parameterMap.put(StudyActionService.class,studies);
+        parameterMap.put(SampleActionService.class,samples);
+        parameterMap.put(AssayActionService.class,assays);
+
+        assayDatas = new AssayData[SUBMITTABLE_COUNT];
+
+        for (int i = 0; i < SUBMITTABLE_COUNT; i++) {
+            samples[i] = TestHelper.getSample(UUID.randomUUID().toString(),team);
+            assays[i] = TestHelper.getAssay(UUID.randomUUID().toString(),team,samples[i].getAlias(),studies[0].getAlias());
+            assayDatas[i] = TestHelper.getAssayData(UUID.randomUUID().toString(),team,assays[i].getAlias());
+            File file = new File();
+            file.setChecksum("abcdefgh12345678abcdefgh12345678");
+            file.setChecksumMethod("MD5");
+            String fileName = UUID.randomUUID().toString() + ".fastq";
+            createTestFile(fileName);
+            java.io.File uploadedFile = uploadFile(ftpClient, fileName);
+            file.setName(fileName);
+            file.setType("fastq");
+            assayDatas[i].getFiles().add(file);
+        }
+
+        final RECEIPTDocument.RECEIPT receipt = fullSubmissionService.submit(submissionAlias,team.getName(),parameterMap);
+        assertThat(receipt.getSuccess(), is(true));
+        for (Submittable submittable : assays) {
+            assertThat(submittable.getAccession(), startsWith("ERX"));
+        }
+    }
+
+    private FTPClient connectToWebinFTP() throws IOException {
+        FTPClient ftp = new FTPClient();
+        ftp.connect(enaFTPServerURL);
+        ftp.enterLocalPassiveMode();
+        int reply = ftp.getReplyCode();
+
+        if (!FTPReply.isPositiveCompletion(reply))  {
+            ftp.disconnect();
+            throw new IOException("FTP server " + enaFTPServerURL + " refused connection");
+        }
+
+        if (!ftp.login(ftpUsername, ftpPassword)) {
+            ftp.logout();
+            throw new IOException("FTP server failed to login using username " + ftpUsername);
+        }
+
+        ftp.setFileType(FTP.BINARY_FILE_TYPE);
+
+        return ftp;
+    }
+
+    private java.io.File uploadFile(FTPClient ftpClient, String fileName) throws IOException {
+        java.io.File file = new java.io.File(fileName);
+        InputStream inputStream = new FileInputStream(file);
+
+        final boolean success = ftpClient.storeFile(file.getName(), inputStream);
+        inputStream.close();
+        if (!success) {
+            throw new RuntimeException("Failed to upload file " + file.getName() + " to " + enaFTPServerURL);
+        }
+        return file;
+    }
+
+    private void createTestFile(String fastaFileName) throws IOException {
+        List<String> lines = Arrays.asList("@SEQ_ID",
+                "GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT",
+                "+",
+                "!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65");
+        Path file = Paths.get(fastaFileName);
+        Files.write(file, lines, Charset.forName("UTF-8"));
     }
 
 }
