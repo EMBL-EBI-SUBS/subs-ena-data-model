942269156f8561a7e87e4c1c89c5aced1cb554b7
Neil Goodgame <neilg@ebi.ac.uk>
2018-03-15 15:49:54 +0000

    Removed getName

--- src/main/java/uk/ac/ebi/subs/ena/action/ActionService.java
+++ src/main/java/uk/ac/ebi/subs/ena/action/ActionService.java
@@ -5,5 +5,4 @@ import uk.ac.ebi.ena.sra.xml.SubmissionType;
 public interface ActionService<T> {
     SubmissionType.ACTIONS.ACTION createActionXML (T submittableObject) throws IllegalArgumentException;
     Class<T> getSubmittableClass ();
-    String getName();
 }
