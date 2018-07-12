721e19cdf641356a27390335c37779108293884c
David Richardson <davidr@ebi.ac.uk>
2018-07-12 09:52:16 +0100

    Marshaller setup for sequence variation analysis

--- src/main/java/uk/ac/ebi/subs/data/submittable/ENAAnalysis.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/ENAAnalysis.java
@@ -1,7 +1,26 @@
 package uk.ac.ebi.subs.data.submittable;
 
 
+import uk.ac.ebi.subs.data.component.Attribute;
+import uk.ac.ebi.subs.data.component.SampleRef;
+import uk.ac.ebi.subs.data.component.StudyRef;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class ENAAnalysis extends AbstractENASubmittable<Analysis> {
+
+    private static final String ASSEMBLY_NAME_ATTRIBUTE = "assembly name";
+    private static final String EXPERIMENT_TYPE_ATTRIBUTE = "experiment type";
+    private static final String PROGRAM_ATRRIBUTE = "program";
+    private static final String PLATFORM_ATTRIBUTE = "platform";
+    private static final String IMPUTATION_ATTRIBUTE = "imputation";
 
     public ENAAnalysis(Analysis analysis) throws IllegalAccessException {
         super(analysis);
@@ -15,4 +34,162 @@ public class ENAAnalysis extends AbstractENASubmittable<Analysis> {
     public Submittable createNewSubmittable() {
         return new Analysis();
     }
+
+    public List<StudyRef> getStudyRefs() {
+        return this.getBaseObject().getStudyRefs();
+    }
+
+    public void setStudyRef(List<StudyRef> studyRefs) {
+        this.getBaseObject().setStudyRefs(studyRefs);
+    }
+
+    public List<SampleRef> getSampleRefs() {
+        return this.getBaseObject().getSampleRefs();
+    }
+
+    public void setSampleRefs(List<SampleRef> sampleRefs) {
+        this.getBaseObject().setSampleRefs(sampleRefs);
+    }
+
+    public String getAssemblyName(){
+        Map<String, Collection<Attribute>> attributes = this.getBaseObject().getAttributes();
+
+        if (attributes.containsKey(ASSEMBLY_NAME_ATTRIBUTE)) {
+            Collection<Attribute> assemblyNames = attributes.remove(ASSEMBLY_NAME_ATTRIBUTE);
+
+            if (!assemblyNames.isEmpty()){
+                return assemblyNames.iterator().next().getValue();
+            }
+        }
+        return null;
+    }
+
+    public void setAssemblyName(String assemblyName){
+        if (assemblyName != null) {
+            setAttribute(ASSEMBLY_NAME_ATTRIBUTE, assemblyName);
+        }
+    }
+
+    public String getExperimentType(){
+        Map<String, Collection<Attribute>> attributes = this.getBaseObject().getAttributes();
+
+        if (attributes.containsKey(EXPERIMENT_TYPE_ATTRIBUTE)) {
+            Collection<Attribute> experimentTypes = attributes.remove(EXPERIMENT_TYPE_ATTRIBUTE);
+
+            if (!experimentTypes.isEmpty()){
+                return experimentTypes.iterator().next().getValue();
+            }
+        }
+        return null;
+    }
+
+    public void setExperimentType(String experimentType){
+        if (experimentType != null) {
+            setAttribute(EXPERIMENT_TYPE_ATTRIBUTE, experimentType);
+        }
+    }
+
+    public List<String> getPrograms() {
+        Map<String, Collection<Attribute>> attributes = this.getBaseObject().getAttributes();
+
+        if (attributes.containsKey(PROGRAM_ATRRIBUTE)) {
+            Collection<Attribute> programAttributes = attributes.remove(PROGRAM_ATRRIBUTE);
+
+            List<String> programs = programAttributes.stream()
+                    .map(Attribute::getValue)
+                    .collect(Collectors.toList());
+            return programs;
+        }
+        return null;
+    }
+
+    public void setPrograms(List<String> programs){
+        if (programs != null) {
+            setAttribute(PROGRAM_ATRRIBUTE, programs);
+        }
+
+    }
+
+    public List<String> getPlatforms(){
+        Map<String, Collection<Attribute>> attributes = this.getBaseObject().getAttributes();
+
+        if (attributes.containsKey(PLATFORM_ATTRIBUTE)) {
+            Collection<Attribute> platformAttributes = attributes.remove(PLATFORM_ATTRIBUTE);
+
+            List<String> platforms = platformAttributes.stream()
+                    .map(Attribute::getValue)
+                    .collect(Collectors.toList());
+            return platforms;
+        }
+
+        return null;
+    }
+
+    public void setPlatforms(List<String> platforms){
+        if (platforms != null) {
+            setAttribute(PLATFORM_ATTRIBUTE, platforms);
+        }
+
+    }
+
+
+    public String getImputation() {
+        Map<String, Collection<Attribute>> attributes = this.getBaseObject().getAttributes();
+
+        if (attributes.containsKey(IMPUTATION_ATTRIBUTE)) {
+            Collection<Attribute> imputationAttributes = attributes.remove(IMPUTATION_ATTRIBUTE);
+
+            Optional<String> imputation = imputationAttributes
+                    .stream()
+                    .map(attribute -> attribute.getValue())
+                    .map(imputationValue -> {
+                        if ("true".equalsIgnoreCase(imputationValue)) {
+                            return "1";
+                        }
+                        if ("false".equalsIgnoreCase(imputationValue)) {
+                            return "0";
+                        }
+                        return null;
+                    })
+                    .filter(Objects::nonNull)
+                    .findAny();
+            return imputation.get();
+        }
+        return null;
+    }
+
+    public void setImputation(String imputation) {
+        if (imputation != null) {
+            if (imputation.equalsIgnoreCase("1")) {
+                setAttribute(IMPUTATION_ATTRIBUTE, "true");
+            }
+            if (imputation.equalsIgnoreCase("0")) {
+                setAttribute(IMPUTATION_ATTRIBUTE, "false");
+            }
+        }
+    }
+
+    private void setAttribute(String name, String value) {
+        this.getBaseObject()
+                .getAttributes()
+                .put(
+                        name,
+                        Arrays.asList(
+                                valueAttribute(value)
+                        )
+                );
+    }
+
+    private static Attribute valueAttribute(String value) {
+        Attribute a = new Attribute();
+        a.setValue(value);
+        return a;
+    }
+
+    private void setAttribute(String name, Collection<String> values) {
+        List<Attribute> attributes = values.stream().map(v -> valueAttribute(v)).collect(Collectors.toList());
+
+        this.getBaseObject().getAttributes().put(name, attributes);
+    }
+
 }
--- src/test/java/uk/ac/ebi/subs/xml/AnalysisSerialisationTest.java
+++ src/test/java/uk/ac/ebi/subs/xml/AnalysisSerialisationTest.java
@@ -14,14 +14,22 @@ import javax.xml.bind.JAXBException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMResult;
+import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class AnalysisSerialisationTest extends SerialisationTest {
 
     private final static String ANALYSIS_RESOURCE = "/uk/ac/ebi/subs/ena/submittable/eva.analysis.json";
+
+    private final static String ASSEMBLY_REF_NAME_XPATH = "/ANALYSIS/ANALYSIS_TYPE/SEQUENCE_VARIATION/ASSEMBLY/STANDARD/@refname";
+    private final static String SEQ_VAR_EXPERIMENT_TYPE_XPATH = "/ANALYSIS/ANALYSIS_TYPE/SEQUENCE_VARIATION/EXPERIMENT_TYPE/text()";
+    private final static String SEQ_VAR_PLATFORM_XPATH = "/ANALYSIS/ANALYSIS_TYPE/SEQUENCE_VARIATION/PLATFORM/text()";
+    private final static String SEQ_VAR_PROGRAM_XPATH = "/ANALYSIS/ANALYSIS_TYPE/SEQUENCE_VARIATION/PROGRAM/text()";
+    private final static String SEQ_VAR_IMPUTATION_XPATH = "/ANALYSIS/ANALYSIS_TYPE/SEQUENCE_VARIATION/IMPUTATION/text()";
 
     @Override
     @Before
@@ -34,13 +42,54 @@ public class AnalysisSerialisationTest extends SerialisationTest {
 
     @Test
     public void testAnalysisSerialisation() throws IOException, IllegalAccessException, JAXBException, ParserConfigurationException, TransformerException {
+        Document document = marshallAnalysisResource();
+        String documentString = getDocumentString(document);
+        logger.info(documentString);
+
+    }
+
+    @Test
+    public void testSequenceVariationAssembly() throws ParserConfigurationException, JAXBException, IllegalAccessException, IOException, XPathExpressionException, TransformerException {
+        Document document = marshallAnalysisResource();
+        String assembly = executeXPathQueryNodeValue(document, ASSEMBLY_REF_NAME_XPATH);
+        assertThat("assembly name serialised to XML", assembly, equalTo("GRCh38"));
+    }
+
+    @Test
+    public void testSequenceVariationExperimentType() throws ParserConfigurationException, JAXBException, IllegalAccessException, IOException, XPathExpressionException, TransformerException {
+        Document document = marshallAnalysisResource();
+        String experimentType = executeXPathQueryNodeValue(document, SEQ_VAR_EXPERIMENT_TYPE_XPATH);
+        assertThat("experiment type serialised to XML", experimentType, equalTo("Genotyping by sequencing"));
+    }
+
+    @Test
+    public void testSequenceVariationPlatform() throws ParserConfigurationException, JAXBException, IllegalAccessException, IOException, XPathExpressionException, TransformerException {
+        Document document = marshallAnalysisResource();
+        String platform = executeXPathQueryNodeValue(document, SEQ_VAR_PLATFORM_XPATH);
+        assertThat("platform serialised to XML", platform, equalTo("Illumina"));
+    }
+
+    @Test
+    public void testSequenceVariationProgram() throws ParserConfigurationException, JAXBException, IllegalAccessException, IOException, XPathExpressionException, TransformerException {
+        Document document = marshallAnalysisResource();
+        String program = executeXPathQueryNodeValue(document, SEQ_VAR_PROGRAM_XPATH);
+        assertThat("program serialised to XML", program, equalTo("GATK"));
+    }
+
+    @Test
+    public void testSequenceVariationImputation() throws ParserConfigurationException, JAXBException, IllegalAccessException, IOException, XPathExpressionException, TransformerException {
+        Document document = marshallAnalysisResource();
+        String imputation = executeXPathQueryNodeValue(document, SEQ_VAR_IMPUTATION_XPATH);
+        assertThat("platform serialised to XML", imputation, equalTo("0"));
+    }
+
+
+    private Document marshallAnalysisResource() throws IOException, IllegalAccessException, ParserConfigurationException, JAXBException {
         final Analysis analysisFromResource = TestHelper.getAnalysisFromResource(ANALYSIS_RESOURCE);
         ENAAnalysis enaAnalysis = new ENAAnalysis(analysisFromResource);
         final Document document = documentBuilderFactory.newDocumentBuilder().newDocument();
         marshaller.marshal(enaAnalysis, new DOMResult(document));
-        final String documentString = getDocumentString(document);
-        logger.info(documentString);
-        assertNotNull(enaAnalysis);
+        return document;
     }
 
     @Override
@@ -52,4 +101,6 @@ public class AnalysisSerialisationTest extends SerialisationTest {
     protected String getName() {
         return "ANALYSIS";
     }
+
+
 }
